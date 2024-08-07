418d5fcf324822abeb6752ee195781b02dd6a9e7
021compa <luizcompan2024@gmail.com>
2024-08-07 07:50:24 -0300

    adição de data e hora + validação

--- galeria/models.py
+++ galeria/models.py
@@ -1,10 +1,12 @@
 from django.db import models
-
+from datetime import datetime
 class Fotografia(models.Model):
 
     nome = models.CharField(max_length=100,)
     foto = models.CharField(max_length=150, null=False, blank=False)
     descricao = models.TextField()
+    publicada = models.BooleanField(default=False)
+    data_Imagem = models.DateTimeField(default=datetime.now , blank = False)
 
     def __str__(self):
         return f'Fotografia [nome={self.nome}]'
@@ -14,6 +16,8 @@ class Caricatura(models.Model):
     nome = models.CharField(max_length=100,)
     foto = models.CharField(max_length=150, null=False, blank=False)
     descricao = models.TextField()
+    publicada = models.BooleanField(default=False)
+    data_Imagem = models.DateTimeField(default=datetime.now , blank = False)
 
     def __str__(self):
         return f'Caricatura [nome={self.nome}]'
